5dfb7c1e0cb662e3927b3cabbe52f9a44ca546f8
UditAwasthi <168617474+UditAwasthi@users.noreply.github.com>
2026-03-07 20:24:58 +0530

    fixed video upload

--- src/utils/uploadToCloudinary.js
+++ src/utils/uploadToCloudinary.js
@@ -2,14 +2,22 @@ import cloudinary from "../config/cloudinary.js";
 
 export const uploadImage = async (file) => {
 
+  const isVideo = file.mimetype.startsWith("video/");
+
   const result = await cloudinary.uploader.upload(
     `data:${file.mimetype};base64,${file.buffer.toString("base64")}`,
     {
       folder: "zynon/profile_photos",
-      transformation: [
-        { width: 500, height: 500, crop: "fill", gravity: "face" },
-        { quality: "auto", fetch_format: "auto" }
-      ]
+      resource_type: isVideo ? "video" : "image",
+
+      ...(isVideo
+        ? {}
+        : {
+            transformation: [
+              { width: 500, height: 500, crop: "fill", gravity: "face" },
+              { quality: "auto", fetch_format: "auto" }
+            ]
+          })
     }
   );
 
